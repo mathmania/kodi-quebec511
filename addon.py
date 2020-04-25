93ec2cf8d44f2707ecd98504181f51065fac40f2
Mathieu Lambert <mathieu.008@hotmail.com>
2020-04-25 17:20:01 -0400

    The addon now can play a single camera video feed

--- addon.py
+++ addon.py
@@ -1,11 +1,22 @@
+import sys
 import xbmcaddon
+import xbmcplugin
 import xbmcgui
  
 addon       = xbmcaddon.Addon()
-addonname   = addon.getAddonInfo('name')
-
-# Set a string variable to use 
-line1 = "Hello World! We can write anything we want here Using Python"
+addonId     = int(sys.argv[1])
 
 # Launch a dialog box in kodi showing the string variable 'line1' as the contents
-xbmcgui.Dialog().ok(addonname, line1)
+url = 'https://www.quebec511.info/camera.ashx?id=3604&format=mp4'
+li = xbmcgui.ListItem('My First Video!', iconImage='DefaultVideo.png')
+video_info = {
+    'codec': 'h264',
+    'aspect': 1.78,
+    'width': 1280,
+    'height': 720,
+}
+li.addStreamInfo('video', video_info)
+xbmcplugin.addDirectoryItem(handle=addonId, url=url, listitem=li)
+
+
+xbmcplugin.endOfDirectory(addonId)
